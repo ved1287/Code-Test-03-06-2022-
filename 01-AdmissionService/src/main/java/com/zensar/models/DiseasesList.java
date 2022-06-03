af625721f0369605b52a3bf631e1cdfb78409f22
ved1287 <101237091+ved1287@users.noreply.github.com>
2022-06-03 11:55:26 +0530

    Add files via upload

---
+++ 01-AdmissionService/src/main/java/com/zensar/models/DiseasesList.java
@@ -0,0 +1,16 @@
+package com.zensar.models;
+
+import java.util.List;
+
+public class DiseasesList {
+	public List<Disease> diseases;
+
+	public List<Disease> getDiseases() {
+		return diseases;
+	}
+
+	public void setDiseases(List<Disease> diseases) {
+		this.diseases = diseases;
+	}
+
+}
